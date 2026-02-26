924b5613724ecca921b22446cd6a11cb7ef49995
Vlad S <vladislav@yourfintech.com>
2026-02-26 15:33:37 +0200

    update position pl update event to batch

--- src/competition_position/competition_positions_pl_update.rs
+++ src/competition_position/competition_positions_pl_update.rs
@@ -3,14 +3,26 @@ use yft_service_sdk::external::my_service_bus_sdk;
 use yft_service_sdk::external::my_service_bus_sdk::macros::my_sb_entity_protobuf_model;
 
 #[derive(Clone, PartialEq, ::prost::Message, Serialize, Deserialize)]
-#[my_sb_entity_protobuf_model(topic_id = "competition-positions-pl-update")]
-pub struct CompetitionPositionsPlUpdateSbEvent {
+#[my_sb_entity_protobuf_model(topic_id = "competition-positions-pl-update-batch")]
+pub struct CompetitionPositionsPlUpdateBatchSbEvent {
     #[prost(string, tag = "1")]
-    pub id: String,
-    #[prost(string, tag = "2")]
     pub contest_id: String,
-    #[prost(string, tag = "3")]
+    #[prost(message, repeated, tag = "2")]
+    pub account_updates: Vec<ContestAccountPositionPlUpdate>,
+}
+
+#[derive(Clone, PartialEq, ::prost::Message, Serialize, Deserialize)]
+pub struct ContestAccountPositionPlUpdate {
+    #[prost(string, tag = "1")]
     pub contest_account_id: String,
-    #[prost(double, tag = "4")]
+    #[prost(message, repeated, tag = "2")]
+    pub positions: Vec<PositionPlUpdate>,
+}
+
+#[derive(Clone, PartialEq, ::prost::Message, Serialize, Deserialize)]
+pub struct PositionPlUpdate {
+    #[prost(string, tag = "1")]
+    pub id: String,  // position_id
+    #[prost(double, tag = "2")]
     pub pl: f64,
 }
